41bccb00774b71045ac7000d5a5462fe72c2c3c9
ひろろ <hrsi.ad@gmail.com>
2021-02-25 21:13:17 +0900

    Initial Commit

---
+++ task14/task14App.swift
@@ -0,0 +1,17 @@
+//
+//  task14App.swift
+//  task14
+//
+//  Created by Hiroshi.Nakai on 2021/02/25.
+//
+
+import SwiftUI
+
+@main
+struct task14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
